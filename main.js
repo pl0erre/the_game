c09a9b013240012fb3048278f47d96267e35fccf
pl0erre <luca.angona@web.de>
2019-07-10 18:30:45 +0200

    Reorganized code, added Board and Game class and connected everything

--- main.js
+++ main.js
@@ -1,13 +1,83 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+  var pauseMessage = "Game paused. Press `Enter` to continue"
+  var game = new Game();
+
+  document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37: // left
+          game.currentShape.x -= 45;
+            break;
+        case 38: // up
+          game.currentShape.rotateShape();
+            break;
+        case 39: // right
+          game.x += 45;
+
+          break;
+        case 40: // down
+          game.y += 45;
+          break;
+        default:
+          // alert("Invalid Key");
+          break;
+    }
+  }
+
+
+
+document.getElementsByClassName("btn-start")[0].addEventListener("click",);
+document.getElementsByClassName("btn-reset")[0].addEventListener("click",);
+document.getElementsByClassName("btn-pause")[0].addEventListener("click", alert(pauseMessage));
+
+
+});
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // 
+
+
+
+
+
+
+
+  /*
 var canvas = document.getElementsByClassName("canvas")[0];
 var ctx = canvas.getContext("2d");
     ctx.scale(1,1);
+
 var shapeOne;
 var randomShape;
 var randomColor;
 var randNr;
+var intervalId;
 
 
   function randShape() {
@@ -26,6 +96,8 @@ var randNr;
     shapeOne.y += boxSize;
   }
 
+
+  // =============================================================
   document.onkeydown = function(e) {
     switch (e.keyCode) {
         case 37: // left
@@ -50,6 +122,10 @@ var randNr;
             break;
     }
   };
+  document.getElementsByClassName("btn-start")[0].addEventListener("click", startGame);
+  document.getElementsByClassName("btn-reset")[0].addEventListener("click", resetGame);
+  //===============================================================
+
 
   function startGame() {
     
@@ -57,7 +133,7 @@ var randNr;
     shapeOne = new Shape(randomShape, 135, 0, randomColor);
     draw();
 
-    setInterval(() =>  {
+    intervalId = setInterval(() =>  {
       
       moveDown();
       draw();
@@ -65,6 +141,11 @@ var randNr;
     }, timer);
   }
 
-  startGame();
+  function resetGame() {  //currently wiping the whole canvas
+    clearInterval(intervalId);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  }
 
 });
+*/
--- script.js
+++ script.js
@@ -68,19 +68,17 @@ class Box {
 class Shape {
   constructor(currentShape = [[]], x, y, color) {
     this.currentShape = currentShape,
-    this.x = x,
-    this.y = y,
     this.color = color,
-    this.shape = this.generateShape(this.currentShape.length)
+    this.shape = this.generateShape(x, y)
   }
 
-  generateShape() {
+  generateShape(x, y) {
     let boxes = [];
     for(let i = 0; i < this.currentShape.length; i++) {
       for(let j = 0; j < this.currentShape[i].length; j++) {
 
         if(this.currentShape[i][j] == 1) {
-          boxes.push(new Box(this.x + j * boxSize, this.y + i * boxSize, boxSize, this.color));
+          boxes.push(new Box(x + j * boxSize, y + i * boxSize, boxSize, this.color));
         }
       }
     }
@@ -88,11 +86,14 @@ class Shape {
   }
 
   draw(ctx) {
-
-    this.shape = this.generateShape();
+    ctx.fillStyle = this.color
     this.shape.forEach(box => ctx.fillRect(box.x, box.y, box.size, box.size))
   }
-  
+
+  fallShape() {
+    this.shape.forEach(box => box.y += box.size)
+  }
+
   rotateShape() {
     let rotate = [];
     for(let i = 0; i < this.currentShape.length; i++) {
@@ -102,6 +103,81 @@ class Shape {
         }
     }
     this.currentShape = rotate;
+  }
+}
+
+
+class Board {
+  constructor() {
+    this.gridOfBoxes = []; 
+  }
+
+  addBoxes(newBoxes) {
+    for(let i = 0; i < newBoxes.length; i++) {
+      this.gridOfBoxes.push(newBoxes[i]);
+    }
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = "#00CC2C";
+    this.gridOfBoxes.forEach(box => ctx.fillRect(box.x, box.y, box.size, box.size));
+  }
+
+  // checks if box already exists
+  doesBoxFit(box) {
+    if (box.y > 765 || box.x == 0 || box.x > 405) return false;
+
+    for (let i = 0; i < this.gridOfBoxes.length; i++) {
+      if (this.gridOfBoxes[i].x == box.x) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+
+class Game {
+  constructor() {
+    this.board = new Board();
+    this.currentShape =  this.getRandShape();
+    this.canvas = document.getElementsByClassName("canvas")[0];
+    this.play();
+  }
+
+  getRandShape() {
+    let randNr = (Math.floor(Math.random()*shapesArr.length));
+    let randomShape = shapesArr[randNr][0];
+    let randomColor = shapesArr[randNr][1];
+    return new Shape(randomShape, 150, -90, randomColor)
+  }
+
+  play() {
+   setInterval(function() {
+     this.draw()
+   }.bind(this), 200)
+  }
+
+  draw() {
+    // be sure that we actually have a falling shape
+    if(this.currentShape === undefined) return; // here you should spawn a new block!
+    let currentShapeBoxesTemp = [].concat(this.currentShape.shape);
+
+
+    this.currentShape.fallShape();
+
+    for (let i = 0; i < this.currentShape.shape.length; i++) {
+          if (!this.board.doesBoxFit(this.currentShape.shape[i])) {
+            this.currentShape = undefined;
+            this.board.addBoxes(currentShapeBoxesTemp); 
+            return;
+      }
+    }
+
+    let ctx = this.canvas.getContext("2d");
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.currentShape.draw(ctx);
+    this.board.draw(ctx)
   }
 
 }
